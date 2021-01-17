66ca45bc101a379f4e7e070b8ef4feacc3f1143e
Artem Karpov <artem.kar2003@gmail.com>
2021-01-17 22:59:24 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/OneWayList.cs
@@ -0,0 +1,91 @@
+﻿using NPOI.SS.Formula.PTG;
+using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class OneWayList
+    {
+        public int Length = 0;
+        public Node FirstNode;
+        Node LastNode;
+        public void Remove(int index)
+        {
+            if(index==0)
+            {
+                LastNode.NextNode = FirstNode.NextNode;
+                FirstNode = FirstNode.NextNode;
+                Length--;
+            }else if(index==this.Length - 1)
+            {
+                Node current = FirstNode;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.NextNode;
+                }
+                current.NextNode = FirstNode;
+                LastNode = current;
+                Length--;
+            }
+            else if (Length > 1)
+            {
+                Node current = FirstNode;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.NextNode;
+                }
+                current.NextNode = current.NextNode.NextNode;
+                Length--;
+            }
+            else
+            {
+                FirstNode = LastNode = null;
+            }
+        }
+        public void Add(int data)
+        {
+            Node tnode = new Node(data);
+            if (this.Length == 0) this.FirstNode = this.LastNode = tnode;
+            else
+            {
+                this.LastNode.NextNode = tnode;
+                this.LastNode = tnode;
+                this.LastNode.NextNode = this.FirstNode;
+            }
+
+            this.Length++;
+        }
+        public void Show()
+        {
+            Node current = FirstNode;
+            for (int i=0;i<this.Length;i++)
+            {
+                Console.Write(current.Data + "| ");
+                current = current.NextNode;
+            }
+            Console.WriteLine();
+        }
+        public int Get(int index)
+        {
+            Node current = FirstNode;
+            for (int i = 0; i < index; i++) 
+            {
+                current = current.NextNode;
+            }
+            return current.Data;
+        }
+
+        public class Node
+        {
+            internal int Data;
+            internal Node NextNode;//pointer
+
+            public Node(int data)
+            {
+                this.Data = data;
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Diagnostics;
+using System.Threading;
+using System.Diagnostics.CodeAnalysis;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using ConsoleApp3;
+using static ConsoleApp3.OneWayList;
+
+namespace ВМ
+{
+    class Program    
+    {        
+        private static void Main()
+        {
+            OneWayList List = new OneWayList();
+            List.Add(1);
+            List.Add(2);
+            List.Show();
+            Console.WriteLine(List.FirstNode.NextNode.NextNode.Data);
+            List.Add(3);
+            List.Show();
+            List.Remove(3);
+            List.Show();
+            Console.WriteLine(List.Get(0));
+            Console.WriteLine(List.Get(1));
+        }
+    }
+}
